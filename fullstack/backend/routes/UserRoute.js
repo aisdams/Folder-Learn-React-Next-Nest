9c4834096bc8653c8d2a6fe8315223d0e8355975
aisdams <marioaistwice@gmail.com>
2022-12-28 16:14:49 +0700

    try for push

---
+++ fullstack/backend/routes/UserRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getUsers,
+  getUsersById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controller/UserController.js ";
+
+const router = express.Router();
+
+router.get("/user", getUsers);
+router.get("/user/:id", getUsersById);
+router.post("/user/", createUser);
+router.patch("/user/:id", updateUser);
+router.delete("/user/:id", deleteUser);
+
+export default router;
